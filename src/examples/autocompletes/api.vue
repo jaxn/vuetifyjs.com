44afda874038c9c4c3f4fd43369fb4beeae4c287
Jackson Miller <jackson@tekzen.net>
2018-08-19 16:30:52 -0500

    Remove trailing whitespace

--- src/examples/autocompletes/api.vue
+++ src/examples/autocompletes/api.vue
@@ -97,7 +97,7 @@
       search (val) {
         // Items have already been loaded
         if (this.items.length > 0) return
-        
+
         // Items have already been requested
         if (this.isLoading) return
 
